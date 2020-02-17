d256149c06c154dbbbe8cc44d9fd5b51ed496203
Bruno Eduardo <bruno97br@gmail.com>
2020-02-17 17:28:35 -0300

    Including method and modules to open and scrap url

--- scraper.py
+++ scraper.py
@@ -1,2 +1,13 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+url_to_parse = "https://www.amazon.com.br"
+
+def open_url_and_soup_it(url):
+    #TODO catch para urllib.error.URLError
+    with urlopen(url) as response:
+        soup = BeautifulSoup(response, 'html.parser')
+    return soup
+
 if __name__ == '__main__':
-    print("Hello world!")
+    print(open_url_and_soup_it(url_to_parse))
