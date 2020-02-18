33ebec0f748200d21b4b2e599a2446924c537a02
Bruno-Eduardo <bruno97br@gmail.com>
2020-02-17 21:33:25 -0300

    Creating a iterator over the database and a meal parser

--- scraper.py
+++ scraper.py
@@ -12,7 +12,32 @@ def open_url_and_soup_it(url):
 
 def get_database(url_to_parse):
     return open_url_and_soup_it(url_to_parse)
+
+def parse_meals(meals):
+    parsed_meals =[]
+    for meal in meals:
+        full_course_dinner = meal.find_all("td")
+
+        parsed_dishes = []
+        for dish in full_course_dinner:
+            if dish.get_text() == " ":
+                continue
+            parsed_dishes.append(dish.get_text())
+        parsed_meals.append(parsed_dishes)
+
+    return parsed_meals
+
+def split_db_by_meal(db):
+    #FIXME "Cafe da manha" is returning empty
+    meals = db.find_all(class_="fundo_cardapio")
+    meals = parse_meals(meals)
+
+    titles = db.find_all(class_="titulo_cardapio")
+    titles = [title.get_text() for title in titles] # filter text
+    return zip(titles, meals)
+
 def main():
     database = get_database(url_to_parse)
+    menu = split_db_by_meal(database)
 if __name__ == '__main__':
     main()
